80ff084d23cf7451d12a36b6b0ad19cb064b7c50
Kayann Soares <kayan.soares1@gmail.com>
2021-05-28 23:45:17 -0300

    Update

--- Signal_Fork/fsignal.c
+++ Signal_Fork/fsignal.c
@@ -6,58 +6,54 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-
-const char *filename = "teste.x";
+const char * filename = "teste.x";
 
 void handler(pid_t);
 
-int main(void){
-	pid_t process_id;
-	pid_t sid = 0;
-  
-    if ((process_id = fork()) < 0){
-        perror("Fork");
-        exit(1);
+int main(void) {
+  pid_t process_id;
+  pid_t sid = 0;
+
+  if ((process_id = fork()) < 0) {
+    perror("Fork");
+    exit(1);
+  }
+
+  if (process_id > 0) {
+    signal(SIGUSR1, handler);
+    for (;;)
+      pause();
+
+  } else {
+    while (1) {
+      if (access(filename, F_OK) == 0) {
+        // Arquivo existe
+        kill(0, SIGUSR1);
+      } else {
+        // Arquivo não existe	
+      }
     }
-    
-    if (process_id > 0){
-		signal(SIGUSR1,handler);
-        for (;;)
-        	pause();
-        
-    }
-    
-    else{
-		while(1){
-	    	if(access(filename, F_OK) == 0){
-	    		// Arquivo existe
-				kill(0,SIGUSR1);		
-			}
-			else{
-				// Arquivo não existe	
-			}
-		}
-    }
+  }
 
-    // Altera as permissões de acesso, Zero significa que as permissões serão herdadas 
-    umask(0);
+  // Altera as permissões de acesso, Zero significa que as permissões serão herdadas 
+  umask(0);
 
- 	// Cria uma nova sessão
-  	sid = setsid();
-  	if(sid < 0){
-    	exit(1);
-  	}
+  // Cria uma nova sessão
+  sid = setsid();
+  if (sid < 0) {
+    exit(1);
+  }
 
- 	// Fechaa a entrada padrão, a saída padrão e a saída de erro padrão
-  	close(STDIN_FILENO);
-  	close(STDOUT_FILENO);
-  	close(STDERR_FILENO);
+  // Fecha a entrada padrão, a saída padrão e a saída de erro padrão
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
 
-  	exit(0);
+  exit(0);
 }
 
-void handler(int sig){
-	signal(SIGUSR1, handler);
-	psignal(sig, "Sinal Recebido");
-	printf("OK\n");
+void handler(int sig) {
+  signal(SIGUSR1, handler);
+  psignal(sig, "Sinal Recebido");
+  printf("OK\n");
 }
